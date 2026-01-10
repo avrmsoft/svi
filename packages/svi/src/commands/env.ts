744d64ad9ac48b4430218a386f3e1ac08057caf3
raaleksandr <raaleksandr@yandex.ru>
2026-01-10 16:35:03 +0100

    Config file paramteter implementation and fixing of the corresponding bugs

--- packages/svi/src/commands/env.ts
+++ packages/svi/src/commands/env.ts
@@ -3,15 +3,26 @@ import fs from "fs";
 import { config as loadEnv } from "dotenv";
 import { DEFAULT_ENV_FILE } from "../utils/constants";
 import { toCamelCase } from "../utils/utils.js";
+import Logger from "../utils/logger.js";
+import { isDirectory } from "../utils/file";
 
 export function enrichOptionsFromEnv(options: any, possibleOptions?: string[]) {
-  if (options.env) {
+  Logger.trace("Enriching options from environment variables");
+
+  tryLoadEnvFromEnvFile(options);
+
+  /*if (options.env) {
     loadEnv({ path: path.resolve(options.env) });
   } else if (fs.existsSync(path.resolve(DEFAULT_ENV_FILE))) {
     loadEnv({ path: path.resolve(DEFAULT_ENV_FILE) });
-  } else if (options.configPath && fs.existsSync(path.join(path.dirname(options.configPath), DEFAULT_ENV_FILE))) {
-    loadEnv({ path: path.join(path.dirname(options.configPath), DEFAULT_ENV_FILE) });
-  }
+  } else if (
+    options.configPath &&
+    fs.existsSync(path.join(path.dirname(options.configPath), DEFAULT_ENV_FILE))
+  ) {
+    loadEnv({
+      path: path.join(path.dirname(options.configPath), DEFAULT_ENV_FILE),
+    });
+  }*/
 
   let possibleKeys: string[] = ["modelName", "apiKey"];
   if (possibleOptions) {
@@ -35,5 +46,85 @@ export function enrichOptionsFromEnv(options: any, possibleOptions?: string[]) {
 
   if (options.apiKey && !options.key) {
     options.key = options.apiKey;
+  }
+}
+
+function tryLoadEnvFromEnvFile(options: any) {
+  if (options.env) {
+    Logger.debug(
+      `Loading environment variables from ${options.env} (set via the corresponding start parameter)`
+    );
+
+    loadEnv({ path: path.resolve(options.env) });
+    return;
+  } else {
+    Logger.trace("No environment file set via start parameter");
+  }
+
+  const pathToDefaultEnvFile = path.resolve(DEFAULT_ENV_FILE);
+  Logger.trace(
+    `Trying to load environment variables from default environment file at ${pathToDefaultEnvFile}`
+  );
+  if (fs.existsSync(path.resolve(DEFAULT_ENV_FILE))) {
+    Logger.debug(
+      `Loading environment variables from default environment file at ${pathToDefaultEnvFile}`
+    );
+    loadEnv({ path: path.resolve(DEFAULT_ENV_FILE) });
+  } else {
+    Logger.trace("No default environment file found");
+  }
+
+  if (!options.configPath) {
+    Logger.trace(
+      "No config path set, therefore we can't load env file from config dir"
+    );
+    return;
+  }
+
+  const pathToEnvFileInConfigDir = path.join(
+    path.dirname(options.configPath),
+    DEFAULT_ENV_FILE
+  );
+
+  Logger.trace(
+    `Trying to load environment variables from environment file in config directory at ${pathToEnvFileInConfigDir}`
+  );
+
+  if (fs.existsSync(pathToEnvFileInConfigDir)) {
+    Logger.debug(
+      `Loading environment variables from environment file in config directory at ${pathToEnvFileInConfigDir}`
+    );
+    loadEnv({
+      path: path.join(path.dirname(options.configPath), DEFAULT_ENV_FILE),
+    });
+    return;
+  } else {
+    Logger.trace(
+      `No environment file found in config directory at path ${pathToEnvFileInConfigDir}`
+    );
+  }
+
+  if (isDirectory(options.configPath)) {
+    const pathToEnvFileInConfigDir2 = path.join(
+      options.configPath,
+      DEFAULT_ENV_FILE
+    );
+
+    Logger.trace(
+      `The provided config path looks like a directory, trying to load environment file from there at ${pathToEnvFileInConfigDir2}`
+    );
+
+    if (fs.existsSync(pathToEnvFileInConfigDir2)) {
+      Logger.debug(
+        `Loading environment variables from environment file in config directory at ${pathToEnvFileInConfigDir2}`
+      );
+      loadEnv({
+        path: pathToEnvFileInConfigDir2,
+      });
+    } else {
+      Logger.trace(
+        `No environment file found in config directory at path ${pathToEnvFileInConfigDir2}`
+      );
+    }
   }
 }
